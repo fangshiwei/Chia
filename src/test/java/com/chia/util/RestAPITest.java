e4af72ca88cb00284d101dd362a225074496a5c2
Fang Shiwei <fangshiwei@outlook.com>
2016-06-14 01:05:59 +0800

    add restapi testing

---
+++ src/test/java/com/chia/util/RestAPITest.java
@@ -0,0 +1,41 @@
+package com.chia.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.client.RestTemplate;
+
+import com.chia.vo.ChiaChargeVO;
+
+public class RestAPITest {
+
+RestTemplate resTemplate;
+	
+	@Before
+	public void setUp() {
+		resTemplate = new RestTemplate();
+	}
+	
+	@Test
+	public void test(){
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testRestJosnToObject(){
+		JSONObject json = new JSONObject();
+		json.put("clientIp", "1.1.1.1");
+		HttpHeaders headers =new HttpHeaders();
+	    headers.setContentType(MediaType.APPLICATION_JSON);
+	    HttpEntity<String> request=new HttpEntity<String>(json.toString(), headers);
+	    ChiaChargeVO chargeVO = resTemplate.postForObject("http://localhost:8080/chia/restful/chiapay/pay",request, ChiaChargeVO.class);
+		assertEquals("1.1.1.1", chargeVO.getClientIp());
+	}
+
+}
