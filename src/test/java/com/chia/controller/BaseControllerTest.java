8db39befd09a4168264155f550a7e4ddada4f51b
fangshiwei <fangshiwei@outlook.com>
2016-07-31 15:53:11 +0800

    Update BaseControllerTest.java
    fixed unit test issue

--- src/test/java/com/chia/controller/BaseControllerTest.java
+++ src/test/java/com/chia/controller/BaseControllerTest.java
@@ -1,6 +1,7 @@
 package com.chia.controller;
 
 import org.junit.runner.RunWith;
+import org.junit.Test;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
